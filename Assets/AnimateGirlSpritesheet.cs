844c97af71a4f13aa79cb49c371256fe5a91bc0d
Cédric B <cedricbardaine@gmail.com>
2020-09-30 16:22:25 +0200

    make roll prioritary

--- Assets/AnimateGirlSpritesheet.cs
+++ Assets/AnimateGirlSpritesheet.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 
-
 using UnityEngine;
 
 public enum LastMovement {
@@ -21,42 +20,35 @@ public class AnimateGirlSpritesheet : MonoBehaviour {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
     void Update() {
-        if (Input.GetKey(KeyCode.RightArrow)) {
+        if (Input.GetKey(KeyCode.Space)) {
+            animator.Play(Anims.Roll);
+            if (lastMove == LastMovement.LEFT) {
+                animator.transform.position += Vector3.left * this.movingSpeed;
+            } else {
+                animator.transform.position += Vector3.right * this.movingSpeed;
+            }
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
             animator.Play(Anims.Run);
-            if(lastMove == LastMovement.LEFT) {
-                spriteRenderer.flipX = !spriteRenderer.flipX ;
+            if (lastMove == LastMovement.LEFT) {
+                spriteRenderer.flipX = !spriteRenderer.flipX;
             }
             lastMove = LastMovement.RIGHT;
-
             animator.transform.position += Vector3.right * this.movingSpeed;
-        } 
-        else if (Input.GetKey(KeyCode.LeftArrow)) {
+        } else if (Input.GetKey(KeyCode.LeftArrow)) {
             animator.Play(Anims.Run);
-            if(lastMove == LastMovement.RIGHT) {
-                spriteRenderer.flipX = !spriteRenderer.flipX ;
+            if (lastMove == LastMovement.RIGHT) {
+                spriteRenderer.flipX = !spriteRenderer.flipX;
             }
             lastMove = LastMovement.LEFT;
-
             animator.transform.position += Vector3.left * this.movingSpeed;
-        } 
-        else if (Input.GetKey(KeyCode.UpArrow)) {
+        } else if (Input.GetKey(KeyCode.UpArrow)) {
             animator.Play(Anims.Run);
             animator.transform.position += Vector3.up * this.movingSpeed;
-        } 
-        else if (Input.GetKey(KeyCode.DownArrow)) {
+        } else if (Input.GetKey(KeyCode.DownArrow)) {
             animator.Play(Anims.Run);
             animator.transform.position += Vector3.down * this.movingSpeed;
-        } 
-        else if (Input.GetKey(KeyCode.Space)) {
-            animator.Play(Anims.Roll);
-            if(lastMove == LastMovement.LEFT) {
-            animator.transform.position += Vector3.left * this.movingSpeed;
-            } else {
-            animator.transform.position += Vector3.right * this.movingSpeed;
-            }
         } else {
             animator.Play(Anims.Iddle);
         }
-    
     }
 }
