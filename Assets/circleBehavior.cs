0e0cfdf427c866d9eec7d4cb9ea486501edc6305
Cédric B <cedricbardaine@gmail.com>
2020-10-21 11:18:13 +0200

    add Circle GameObject & create and attach a script to make it move randomly

---
+++ Assets/circleBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class circleBehavior : MonoBehaviour {
+
+
+  // Start is called before the first frame update
+  void Start() {
+    InvokeRepeating("moveRandomly", 0, 2);
+  }
+
+  // Update is called once per frame
+  void Update(){
+  }
+
+  void moveRandomly() {
+    this.transform.position += new Vector3(
+      UnityEngine.Random.Range(-1f,1f),
+      UnityEngine.Random.Range(-1f,1f),
+      UnityEngine.Random.Range(-1f,1f)
+    );
+
+  }
+}
