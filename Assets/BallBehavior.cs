bae2bba4ab4a643a017d41e0b8aec522854282c0
Cédric B <cedricbardaine@gmail.com>
2020-10-28 16:40:24 +0100

    WIP: handle ball behavior

--- Assets/BallBehavior.cs
+++ Assets/BallBehavior.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class BallBehavior : MonoBehaviour
 {
   private Rigidbody2D rigidbody2D;
+  private SpriteRenderer sRdr;
+  private bool colored = false;
 
 
   void Start()
   {
     rigidbody2D = GetComponent<Rigidbody2D>();
+    sRdr = GetComponent<SpriteRenderer>();
   }
 
-  void Update()
+  void FixedUpdate()
   {
+    if(colored) {
+      // sRdr.color = Color.green;
+      colored = false;
+    }
+  }
 
+  private void OnCollisionEnter2D(Collision2D other)
+  {
+    if (other.gameObject.layer == LayerMask.NameToLayer("Goal"))
+    {
+      sRdr.color = Color.red;
+      colored = true ;
+    }
   }
 }
